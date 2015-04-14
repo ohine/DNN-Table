5a7d2bfcb44fef052170d5cbc778164b34b213ce
cmarfia <marfiadocs@gmail.com>
2015-04-13 22:12:37 -0500

    Added some of the init method body.
    - Error Validation
    - private member setup
    - remove all child elements,
    - added dnnTable classes
    - added destory method

--- Website/Resources/Libraries/dnn-table/01_00_00/dnn-table.js
+++ Website/Resources/Libraries/dnn-table/01_00_00/dnn-table.js
@@ -2,16 +2,69 @@
 (function ($) {
 	'use strict';
 
-	var DnnTable = function (element, options) {
+	var instanceId = 1,
+	DnnTable = function (element, options) {
+		var self = this;
+
+		// Currently only supporting the table element.
+		if (element.nodeName !== "TABLE") {
+			throw 'Element not supported. Trying using a <table></table> element.';
+		}
+
+		// Verify that initial data is supplied.
+		if (typeof options === 'undefined' ||
+			(typeof options.data === 'undefined' &&
+			(typeof options.ajax === 'undefined' ||
+			typeof options.ajax.load === 'undefined'))) {
+			throw 'Initial data not supplied. Use "data" or "ajax": { "load": {} }';
+		}
+		
+		self.options = options;
+		self.element = element;
+		self.instanceId = instanceId++;
+
+		// Remove all child elements.
+		while (self.element.lastChild) {
+			self.element.removeChild(self.element.lastChild);
+		}
+
+		// Add a class to define the instance of the dnnTable on the page.
+		if (self.element.classList) {
+			self.element.classList.add('dnnTable');
+			self.element.classList.add('dnnTable-id-' + self.instanceId);
+		} else {
+			self.element.className += 'dnnTable';
+			self.element.className += 'dnnTable-id-' + self.instanceId;
+		}
+		
+		self.load(options.data || options.ajax.load, options.totalRecordCount);
 	},
 	dnnTableNoConflict = $.fn.dnnTable;
 
 	DnnTable.DEFAULTS = {
+		'columns': [],
+		'serverSide': false,
+		'sorting': false,
+		'paging': false,
+		'searching': false,
+		'advancedSearch': false,
+		'editable': false,
+		'theme': 'default'
 	};
 
 	DnnTable.VERSION = '1.0.0';
 
 	DnnTable.prototype.load = function (inputData, totalRecordCount) {
+		var self = this;
+	};
+	
+	DnnTable.prototype.destory = function () {
+		var self = this;
+		
+		// Remove all child elements.
+		while (self.element.lastChild) {
+			self.element.removeChild(self.element.lastChild);
+		}
 	};
 
 	function DnnTablePlugin(option, inputData) {
